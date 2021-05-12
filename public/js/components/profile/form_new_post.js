6eea588904f88fe9bf8defabe49a1f6d93befc94
Ju Josh Kim <jktpk91@gmail.com>
2021-05-12 16:05:39 -0700

    normalized code format for all forms; added return in render function for button_like.js and comment.js

--- public/js/components/profile/form_new_post.js
+++ public/js/components/profile/form_new_post.js
@@ -1,10 +1,27 @@
 class NewPostForm extends React.Component{
     render = () => {
         return <form onSubmit={this.props.createPost}>
-            <input type="text" name="author" hidden /><br/>
-            <input type="text" name="image" placeholder="Image URL" onKeyUp={this.props.handleChange} /><br/>
-            <input type="text" name="caption" placeholder="Start typing your caption here" onKeyUp={this.props.handleChange} /><br/>
-            <input type="submit" value="Create Post" />
+            <input
+                type="hidden"
+                name="author"
+                value="currentUser" // ADD CURRENT USER TO THIS
+            /><br/>
+            <input
+                type="text"
+                name="image"
+                placeholder="Image URL"
+                onKeyUp={this.props.handleChange}
+            /><br/>
+            <input
+                type="text"
+                name="caption"
+                placeholder="Start typing your caption here"
+                onKeyUp={this.props.handleChange}
+            /><br/>
+            <input
+                type="submit"
+                value="Create Post"
+            />
         </form>
     }
 }
