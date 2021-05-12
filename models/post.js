f2258d4eb06b25db4f8c5847bfeaed1f5dda6302
Ju Josh Kim <jktpk91@gmail.com>
2021-05-11 18:45:43 -0700

    changed format of dateCreated in post model to be slightly more readable

--- models/post.js
+++ models/post.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const postSchema = new Schema({
-  author: String,
-  image: { type: String, required:true },
-  caption: String,
-  dateCreated: { type: Date, default: Date(Date.now) },
-  likedBy: [String],
-  comments: [String]
+    author: String,
+    image: { type: String, required:true },
+    caption: String,
+    dateCreated: {
+        type: Date,
+        default: Date(Date.now)
+    },
+    likedBy: [String],
+    comments: [String]
 })
 
 const Post = mongoose.model('Post', postSchema)
