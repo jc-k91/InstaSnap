98f8e78fbf81b98e9c5da2c3dc9e294263676daf
cheisentrout <cheisentrout@gmail.com>
2021-05-12 12:36:17 -0400

    Gotta add files edited locally so I can pull remote working version

--- public/js/components/profile.js
+++ public/js/components/profile.js
@@ -1,8 +1,13 @@
 // props needed:
 //     user's _id (not username) to call user info to render onto page
+// username, profilePic, bio
 
 class UserProfile extends React.Component {
     render = () => {
-
+      return <section>
+        <h1>{this.props.username}</h1>
+        <img src={this.props.profilePic} alt={this.props.username} />
+        <p>{this.props.bio}</p>
+      </section>
     }
 }
