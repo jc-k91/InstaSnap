1a7d0ff20063f4135d71a0927c380fdea034f9d5
Ju Chan Kim <52368161+jc-k91@users.noreply.github.com>
2021-05-13 07:10:11 -0700

    Revert "Revert "Added Bootstrap form classes to user login form""

--- public/js/components/sessions/form_login.js
+++ public/js/components/sessions/form_login.js
@@ -1,16 +1,20 @@
 class LoginForm extends React.Component {
     render = () => {
-        return <form onSubmit={this.props.login}>
+        return <form onSubmit={this.props.login} className="form-group">
             <h4>User Login</h4>
             <input
                 type="text"
                 name="username"
+                placeholder="username"
                 onKeyUp={this.props.handleChange}
+                className="form-control"
             /><br/>
             <input
                 type="password"
                 name="password"
+                placeholder="password"
                 onKeyUp={this.props.handleChange}
+                className="form-control"
             /><br/>
             <input
                 type="submit"
