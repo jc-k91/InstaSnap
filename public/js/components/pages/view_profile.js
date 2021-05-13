69d3e66b265749572baed3f4e6e0958452aea7e7
Ju Josh Kim <jktpk91@gmail.com>
2021-05-13 16:20:19 -0700

    pulled single post view out of grid view and into profile page; lifted activePost state from grid view to profile page

--- public/js/components/pages/view_profile.js
+++ public/js/components/pages/view_profile.js
@@ -1,6 +1,17 @@
 // Profile, Grid
 
 class ProfileView extends React.Component{
+    state = {
+        activePost: {}
+    }
+    showActivePost = (postObject) => {
+        this.setState(
+            {
+                activePost: postObject
+            }
+        )
+        document.getElementById('post-modal').classList.toggle('hide')
+    }
     render = () => {
         return <div className="profile-page">
             <button onClick={this.props.logout}>Log Out</button>
@@ -9,12 +20,16 @@ class ProfileView extends React.Component{
             ></UserProfile>
             <GridView
                 userPosts={this.props.currentUser.posts}
+                updateActivePost={this.showActivePost}
             ></GridView>
             <NewPostForm
                 handleChange={this.props.handleChange}
                 createPost={this.props.createPost}
                 currentUser={this.props.currentUser}
             ></NewPostForm>
+            <PostEach
+                activePost={this.state.activePost}
+            ></PostEach>
         </div>
     }
 }
