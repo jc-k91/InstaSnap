8825dab24c02379e5d4a27b4816d2f86040b3f34
cheisentrout <cheisentrout@gmail.com>
2021-05-14 10:58:46 -0400

    Changed a few post edit related files

--- public/js/components/post/post.js
+++ public/js/components/post/post.js
@@ -13,6 +13,7 @@ class PostEach extends React.Component {
                 <h4>{this.props.activePost.author}</h4>
                 <img src={this.props.activePost.image} />
                 <p>{this.props.activePost.caption}</p>
+                <EditForm></EditForm>
             </div>
         </section>
     }
