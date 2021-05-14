485f55676240675588ed36483756acae4db34b2a
Ju Josh Kim <jktpk91@gmail.com>
2021-05-13 19:34:43 -0700

    updated nested prop names for all of the landing view

--- public/js/components/sessions/form_create_account.js
+++ public/js/components/sessions/form_create_account.js
@@ -1,29 +1,29 @@
 class CreateAccount extends React.Component {
     render = () => {
-        return <form onSubmit={this.props.createAccount}>
+        return <form onSubmit={this.props.createAccount1}>
             <input
                 type="text"
                 placeholder="Username"
                 name="username"
-                onKeyUp={this.props.handleChange}
+                onKeyUp={this.props.handleChange1}
             />
             <input
                 type="password"
                 placeholder="Password"
                 name="password"
-                onKeyUp={this.props.handleChange}
+                onKeyUp={this.props.handleChange1}
             />
             <input
                 type="text"
                 placeholder="Profile Pic URL"
                 name="profilePic"
-                onKeyUp={this.props.handleChange}
+                onKeyUp={this.props.handleChange1}
             />
             <input
                 type="text"
                 placeholder="Bio"
                 name="bio"
-                onKeyUp={this.props.handleChange}
+                onKeyUp={this.props.handleChange1}
             />
             <input type="submit" value="Create Account"/>
         </form>
