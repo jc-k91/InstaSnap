4927dcb736d0f42b74b99328b3b87f1549f57f62
cheisentrout <cheisentrout@gmail.com>
2021-05-13 20:54:28 -0400

    commented app.js

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,6 @@
+/* DOCUMENT NOTES! */
+/* This post controller exists to direct all routes that create data in the back end. Whenever you visit one of these routes, it affects or interacts with the database in some way. */
+
 // ========== DEPENDENCIES ==========
 // ------ EXPRESS ------
 const express = require('express')
@@ -9,6 +12,7 @@ const User = require('../models/user.js')
 
 // ========== ROUTES ==========
 // ------ ALL POSTS ------
+/* This route provides access to all of the posts in our database's posts collection. It returns all found data in JSON format. */
 posts.get('/', (req, res) => {
     Post.find({}, (err, allPosts) => {
         res.json(allPosts)
@@ -16,12 +20,17 @@ posts.get('/', (req, res) => {
 })
 
 // ------ CREATE POST ------
+/* When the user wants to create a post, the router first queries the users collection, and finds the user by searching the req.session.currentUser's auto-generated _id. Req.session.currentUser represents the currently logged in user. */
+/* Once the user has been found by the logged in users's id, create a piece of data in our posts collection, which will consist of the req.body. */
 posts.post('/', (req, res) => {
     User.findById(req.session.currentUser._id, (err1, foundUser) => {
         Post.create(req.body, (err2, createdPost) => {
+            // Send the created post to the front of the found user's posts array!
             foundUser.posts.unshift(createdPost)
+            // ???????
             foundUser.save()
             console.log(foundUser.posts);
+            // Finally, find all the users (but really it's just the one user since this query is nested inside a findById call) 
             User.find({}, (err3, allUsers) => {
                 res.json(allUsers)
             })
--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,25 @@
+// HERE IS THE APP!
+// App is the custom React component that will hold all of the render-able information for this app.
+
 class App extends React.Component{
+  /* State represents certain values of the App, which can be updated.
+     - loggedIn begins as false, and is changed by the login function once it's triggered by a form on the landing page.
+     - currentView begins as profile --- this isn't fully done yet
+     - allUsers is an array
+     - currentUser will be updated with a user's data, triggered by the login function from the landing page */
     state = {
         loggedIn: false,
         currentView: "profile",
         allUsers: [],
         currentUser: {}
     }
+
     // ========== FUNCTIONS ==========
     // ------ USER ACCOUNT ------
+    /* createAccount is a method that belongs to the App component.
+       It queries the database at the /users route, and intakes the component's current state.
+          !!! How does this.state translate into a new piece of user data?! !!!
+       Then, it takes the response from the database and console logs the response. */
     createAccount = () => {
         axios.post(
             '/users',
@@ -15,12 +28,21 @@ class App extends React.Component{
             console.log(response) // THIS IS WHERE WE SHOULD AUTOMATICALLY LOG THE USER IN WITH THE NEW USER CREDENTIALS
         })
     }
+    // This App method hits the delete method in our database, and deletes an account.
     deleteAccount = () => {
         axios.delete(
 
         )
     }
+
     // ------ SESSION ------
+    /* This is the login method for the App.
+       - First, it prevents a form's default behavior of reseting on page refresh, ensuring that the user-entered data is passed through.
+       - Then, it hits the database's post route at session/login, with this.state (again, what is this.state?).
+       - Next, it takes the response from the database, and checks a conditional: if the app current has a logged in user (and the user is not an empty object), then update the state of the app as follows:
+       - Set loggedIn to true
+       - Set the App's currentUser to the database response
+       - Set the current author to the username from the database response (this will allow all created posts to automatically be pushed into a user's post array.) */
     login = (e) => {
         e.preventDefault()
         axios.post(
@@ -38,6 +60,9 @@ class App extends React.Component{
             }
         })
     }
+
+    /* This is the App's logout method.
+       - It hits the session controller's delte route (which destroys the session), then takes the database response and updates the state of the App for loggedIn to be false and the currentUser to be an empty object. */
     logout = () => {
         axios.delete(
             '/session'
@@ -52,6 +77,10 @@ class App extends React.Component{
     }
 
     // ------ POSTS ------
+    /* This is the App method that creates a new post.
+       - First, it prevents the form's default behavior so we don't lose the user's entered data when the form sends.
+       - Then it hits the router's postController and sends the current state of the form through as an argument for creating a piece of post data.
+       - We send this.state because it takes into account all of the current data from the app, which is then parsed by our posts Schema, which saves only what it needs from the current state. this.state replaces req.body */
     createPost = (e) => {
         e.preventDefault()
         axios.post(
@@ -71,6 +100,10 @@ class App extends React.Component{
             })
         })
     }
+    /* This is our App's edit post method.
+       - it makes a put request to our database, and hits the posts controller's posts route, concatenated with the event target's name -- the event target in this case is the selected post.
+       - We pass it { new: true } to make sure we can see the updated post right away.
+       - Then it queries the database with the updated post included and makes sure that allPosts now includes the updated response data. */
     editPost = (e) => {
         e.preventDefault()
         axios.put(
@@ -87,6 +120,7 @@ class App extends React.Component{
             }
         )
     }
+    /*  */
     deletePost = (e) => {
         e.preventDefault()
         // console.log(e.target.value) // What the Farquad. This won't pull the value attritube for some reason...
@@ -103,16 +137,13 @@ class App extends React.Component{
             )
         })
     }
+
     renderProfile = () => {
         this.setState(
             {
-                pageView: "profile"
+                currentView: "profile"
             }
         )
-    }
-
-    renderFunction = (pageViewComponent) => {
-        this.render(pageViewComponent)
     }
 
     // ------ SETTING STATE TO FORM INPUT ------
